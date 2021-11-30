b6f42c7820a9d01d93a58debaf199ce720a1546e
Nivedhan <nivedhan1998@gmail.com>
2021-11-30 22:17:54 +0530

    git commit Model Deployment .py

--- Model Deployment .py
+++ Model Deployment .py
@@ -1,43 +1,28 @@
 import pickle
-
+import gradio as gr
 gaussian = pickle.load(open('Admin.sav','rb'))
 y_pred = gaussian.predict(xtest)
 output = pd.DataFrame(y_pred)
-
 print(output)
 
                               '2-GRADIO'
-                            '1-REGRESSION'
-import gradio as gr
-import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
-#load the dataset to pandas dataframe
-URL = "http://bit.ly/w-data"
-student_data = pd.read_csv(URL)
-#Prepare data
-X = student_data.copy()
-y = student_data['Scores']
-del X['Scores']
-#create a machine learning model and train it
-lineareg = LinearRegression()
-lineareg.fit(X,y)
-print('Accuracy score : ',lineareg.score(X,y),'\n')
-#now the model has been trained well let test it
-#function to predict the input hours
-def predict_score(hours):
-    hours = np.array(hours)
-    pred_score = lineareg.predict(hours.reshape(-1,1))
-    return np.round(pred_score[0], 2)
-input = gr.inputs.Number(label=['Number of Hours studied'])
-output = gr.outputs.Textbox(label='Predicted Score')
-gr.Interface( fn=predict_score,
-              inputs=input,
-              outputs=output).launch(share=True)
+#1-REGRESSION
+
+def prediction(OrganicSearches,Users):
+    # Making predictions
+    prediction = Orders.predict([[OrganicSearches,Users]])
+    return prediction
+
+OrganicSearch = gr.inputs.Slider(minimum=0, maximum=120, default=22, label="OS")
+Users = gr.inputs.Slider(minimum=0, maximum=1000, default=100, label="Fare (british pounds)")
+
+output = gr.outputs.Textbox(label='Weborders')
+gr.Interface( fn=prediction,
+              inputs =  [OrganicSearch,Users],outputs= output,live=True).launch(share=True)
 
 
 
-                        "2-CLASSIFICATION"
+#2-CLASSIFICATION
 import gradio as gr
 import pandas as pd
 import numpy as np
