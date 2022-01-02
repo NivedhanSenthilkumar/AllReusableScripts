74ef12e55d5f606a407a7892d26b633abb167238
Nivedhan <nivedhan1998@gmail.com>
2022-01-02 10:32:32 +0530

    git commit Deep Learning.py

--- Deep Learning.py
+++ Deep Learning.py
@@ -1,12 +1,65 @@
-...
+
+# multi-class classification with Keras
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+
+
+                               "1-CLASSIFICATION"
+                            "1.1-BINARY CLASSIFICATION"
 # define the keras model
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-...
 
-...
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-..
+
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+
+
+                                "1.1-MULTICLASS CLASSIFICATION"
+
+# define baseline model
+def baseline_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(8, input_dim=4, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam',
+                                                 metrics=['accuracy'])
+    return model
+
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=0)
+kfold = KFold(n_splits=10, shuffle=True)
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean() * 100, results.std() * 100))
+
+
+                                "2-REGRESSION"
+#BUILDING MODEL
+model = models.Sequential()
+model.add(layers.Dense(8, activation='relu', input_shape=[X_train.shape[1]]))
+model.add(layers.Dense(16, activation='relu'))
+# output layer
+model.add(layers.Dense(1))
+
+#COMPILING MODEL
+model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+
+#FITTING AND PREDICTIONS
+history = model.fit(X_train_scaled, y_train, validation_split=0.2, epochs=100)
+predictions = model.predict(to_predict)
+
+#EVALUATION
+model.evaluate(X_test_scaled, y_test)
