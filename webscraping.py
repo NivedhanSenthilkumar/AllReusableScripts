871e2bd4a39278bafece68ff3f9a6ffac02b80e4
Nivedhan <nivedhan1998@gmail.com>
2022-01-04 12:26:52 +0530

    git commit webscraping.py

---
+++ webscraping.py
@@ -0,0 +1,15 @@
+"LIBRARIES"
+import requests
+from bs4 import BeautifulSoup
+
+
+
+#1-Extract Facebook Likes
+for i in user['USERNAME']:
+    url = 'https://www.facebook.com/'+ i
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,'lxml')
+    f = soup.find('div', attrs={'class': '_4-u3 _5sqi _5sqk'})
+    if f != None:
+        likes=f.find('span',attrs={'class':'_52id _50f5 _50f7'}) #finding span tag inside class
+        print(i,likes.text)
