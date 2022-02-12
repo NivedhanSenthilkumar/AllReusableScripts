3e33b080e3dc866d714bff87481a5ac21afb7a8c
Nivedhan <nivedhan1998@gmail.com>
2022-02-12 11:05:51 +0530

    git commit Exploratory Data Analysis (EDA).py

--- Custom Functions.py
+++ Custom Functions.py
@@ -63,19 +63,7 @@ def pricesplit(x):
 
 
 
-#Writing to excel
-import pandas as pd
-df1 = pd.DataFrame({'Data': ['a', 'b', 'c', 'd']})
-df2 = pd.DataFrame({'Data': [1, 2, 3, 4]})
-df3 = pd.DataFrame({'Data': [1.1, 1.2, 1.3, 1.4]})
 
-writer = pd.ExcelWriter('multiple.xlsx', engine='xlsxwriter')
-
-df1.to_excel(writer, sheet_name='Sheeta')
-df2.to_excel(writer, sheet_name='Sheetb')
-df3.to_excel(writer, sheet_name='Sheetc')
-
-writer.save()
 
 ##--ALITER
 import csv
