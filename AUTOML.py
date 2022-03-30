305309723f4c00cd5ac09a3d81dd3b444a54fe7c
Nivedhan Senthilkumar <70594428+NivedhanSenthilkumar@users.noreply.github.com>
2022-03-30 14:58:58 +0530

    git commit AUTOML.py

--- AUTOML.py
+++ AUTOML.py
@@ -121,9 +121,21 @@ y_hat = model.predict(X_test)
 model.show_models()
 
 #2-REGRESSION
+X, y = sklearn.datasets.load_diabetes(return_X_y=True)
+X_train, X_test, y_train, y_test = \sklearn.model_selection.train_test_split(X, y, random_state=1)
 
+automl = autosklearn.regression.AutoSklearnRegressor(
+    time_left_for_this_task=120,
+    per_run_time_limit=30,
+    tmp_folder='/tmp/autosklearn_regression_example_tmp',
+)
+automl.fit(X_train, y_train, dataset_name='diabetes')
+print(automl.leaderboard())
 
-
+train_predictions = automl.predict(X_train)
+print("Train R2 score:", sklearn.metrics.r2_score(y_train, train_predictions))
+test_predictions = automl.predict(X_test)
+print("Test R2 score:", sklearn.metrics.r2_score(y_test, test_predictions))
 
 
 
