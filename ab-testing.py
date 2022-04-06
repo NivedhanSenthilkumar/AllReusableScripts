4839793860451bb1a39a95ec866ebaa8e757ac7a
Nivedhan Senthilkumar <70594428+NivedhanSenthilkumar@users.noreply.github.com>
2022-04-06 17:24:56 +0530

    git commit ab-testing.py

--- ab-testing.py
+++ ab-testing.py
@@ -1,1 +1,74 @@
-DDD
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.special import binom
+from scipy.stats import chi2_contingency, mannwhitneyu, norm, t, ttest_ind
+%matplotlib inline
+
+
+                                   """Discrete Metrics"""
+#Let's consider first discrete metrics, e.g. click-though rate. We randomly show visitors one of two possible designs of an advertisement, and based on how many of them click on it we need to determine whether our data significantly contradict the hypothesis that the two designs are equivalently efficient.
+np.random.seed(42)
+x = np.random.binomial(n=1, p=0.6, size=15)
+y = np.random.binomial(n=1, p=0.4, size=19)
+_, (a, c) = np.unique(x, return_counts=True)
+_, (b, d) = np.unique(y, return_counts=True)
+
+df = pd.DataFrame(data=[[a, b], [c, d]],
+                 index=["click", "no click"],
+                 columns=["A", "B"])
+m = df.values
+print("- Observations:")
+print(f"  - Version A: = {x}")
+print(f"  - Version B: = {y}")
+print("")
+print("- Contingency table:")
+display(df)
+
+
+                            1.1 """Fisher's exact test"""
+#Since we have a 2x2 contingency table we can use Fisher's exact test to compute an exact p-value and test our hypothesis.
+def hypergeom(k, K, n, N):
+    """Probability mass funciton of the hypergeometric distribution."""
+    return binom(K, k) * binom(N-K, n-k) / binom(N, n)
+
+
+def fisher_prob(m):
+    """Probability of a given observed contingency table according to Fisher's exact test."""
+    ((a, b), (c ,d)) = m
+    k = a
+    K = a+b
+    n = a+c
+    N = a+b+c+d
+    return hypergeom(k, K, n, N)
+
+def fisher_probs_histogram(m):
+    """Computes prob mass function histogram accroding to Fisher's exact test."""
+    neg_val = -min(m[0,0], m[1,1])
+    pos_val = min(m[1,0], m[1,0])
+    probs = []
+    for k in range(neg_val, pos_val+1):
+        m1 = m + np.array([[1, -1], [-1, 1]]) * k
+        probs.append(fisher_prob(m1))
+    return probs
+
+bars_h = np.array(fisher_probs_histogram(m))
+
+f, ax = plt.subplots(figsize=(6, 3))
+ii = np.arange(len(bars_h))
+ax.bar(ii, bars_h)
+idxs = bars_h <= fisher_prob(m)
+ax.bar(ii[idxs], bars_h[idxs], color='r')
+ax.set_ylabel("prob density")
+p_val = bars_h[idxs].sum()
+neg_val = -min(m[0,0], m[1,1])
+pos_val = min(m[1,0], m[1,0])
+ax.bar(ii[-neg_val], bars_h[-neg_val], color='orange')
+
+ax.set_xticks(ii)
+ax.set_xticklabels(np.arange(neg_val, pos_val+1))
+f.tight_layout()
+print(f"- Fisher's exact test: p-val = {100*p_val:.1f}%")
+
+
