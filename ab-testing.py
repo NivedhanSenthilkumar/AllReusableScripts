7522c65101a989c109f001cba2c91c1f7b9cad82
Nivedhan Senthilkumar <70594428+NivedhanSenthilkumar@users.noreply.github.com>
2022-04-06 17:28:03 +0530

    git commit ab-testing.py

--- ab-testing.py
+++ ab-testing.py
@@ -72,3 +72,49 @@ f.tight_layout()
 print(f"- Fisher's exact test: p-val = {100*p_val:.1f}%")
 
 
+                         """ 1.2 Pearson's chi-squared test """
+#Fisher's exact test has the important advantage of computing exact p-values. But if we have a large sample size, it may be computationally inefficient. In this case, we can use Pearson's chi-squared test to compute an approximate p-value.
+chi2_val, p_val = chi2_contingency(m, correction=False)[:2]
+print("- Pearson's chi-squared t-test:")
+print(f"   - χ2 value: {chi2_val:.3f}")
+print(f"   - p-value: {p_val*100:.1f}%")
+
+
+                              """2. Continuous metrics"""
+#Let's now consider the case of a continuous metrics, e.g. average revenue per user. We randomly show visitors of our website one of two possible layouts of products for sale, and based on how much revenue each user generated in a month we want to determine whether our data significantly contradict the hypothesis that the two website layouts are equivalently efficient.
+np.random.seed(42)
+n_x, n_y = 17, 14
+d1 = norm(loc=200, scale=100)
+d2 = norm(loc=280, scale=90)
+disc = 50
+x = (d1.rvs(size=n_x) / disc).astype(int) * disc
+y = (d2.rvs(size=n_y) / disc).astype(int) * disc
+print("- Observations:")
+print(f"  - Version A: = {x}")
+print(f"  - Version B: = {y}")
+print("")
+print(f"- Distribution plot:")
+
+f, ax = plt.subplots(figsize=(6, 3))
+for i, (x_, l_, c_) in enumerate(zip([x, y], ["A", "B"], ["tab:blue", "tab:olive"])):
+    v, c = np.unique(x_, return_counts=True)
+    ax.bar(v-5+10*i, c, width=10, label=l_, color=c_)
+
+ax.set_xlabel("purchase in $")
+ax.set_ylabel("count")
+ax.legend();
+
+def plot_pval(distribution, t_val, xlims=(-5, 5), ylims=(0, 0.5)):
+    xxx = np.linspace(*xlims, 1000)
+    f, ax = plt.subplots(figsize=(4,3))
+    ax.plot(xxx, distribution.pdf(xxx))
+    ax.set_ylim(ylims)
+    ax.vlines(t_val, 0, stat_distrib.pdf(t_val), color='orange')
+    ax.plot(t_val, stat_distrib.pdf(t_val), 'o', color='orange')
+    xp = xxx <= t_val
+    ax.fill_between(xxx[xp], xxx[xp] * 0, stat_distrib.pdf(xxx[xp]), color='r')
+    xp = xxx >= -t_val
+    ax.fill_between(xxx[xp], xxx[xp] * 0, stat_distrib.pdf(xxx[xp]), color='r')
+    ax.set_ylabel("prob denisty")
+    f.tight_layout()
+    return f, ax
