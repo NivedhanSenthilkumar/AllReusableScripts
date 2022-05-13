e631d10f4754c0afa3c8515323a202c308ff5628
Nivedhan Senthilkumar <70594428+NivedhanSenthilkumar@users.noreply.github.com>
2022-05-13 19:44:39 +0530

    git commit Machine Learning.py

--- Machine Learning.py
+++ Machine Learning.py
@@ -2,6 +2,30 @@
 "IMPORT LIBRARIES"
 from sklearn import svm, datasets
 from sklearn.model_selection import GridSearchCV
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+                            "MODEL INTERPRETABILITY - BASELINE MODELLING"
+                                            "1-REGRESSION"
+cdf = pd.DataFrame(regression.coef_, X.columns, columns=['Coefficients'])
+print(cdf)
+
+# Create a numpy array of data:
+x = np.array([6, 16, 26, 36, 46, 56]).reshape((-1, 1))
+y = np.array([4, 23, 10, 12, 22, 35])
+
+# Create an instance of a linear regression model and fit it to the data with the fit() function:
+model = LinearRegression().fit(x, y)
+# The following section will get results by interpreting the created instance:
+# Obtain the coefficient of determination by calling the model with the score() function, then print the coefficient:
+r_sq = model.score(x, y)
+print('coefficient of determination:', r_sq)
+# Print the Intercept:
+print('intercept:', model.intercept_)
+# Print the Slope:
+print('slope:', model.coef_)
+
 
                            """"SUPERVISED LEARNING"""
                            "1-FEATURE ENGINEERING"
