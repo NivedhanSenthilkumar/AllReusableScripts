69665de0b7d9d6068e179aeca588d83429d108a7
Nivedhan Senthilkumar <nivedhan1998@gmail.com>
2022-10-02 12:40:13 +0100

    Update AB-TESTING.py

--- AB-TESTING.py
+++ AB-TESTING.py
@@ -1,11 +1,36 @@
 
                                                       """LIBRARIES"""
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 from scipy.special import binom
 from scipy.stats import chi2_contingency, mannwhitneyu, norm, t, ttest_ind
 %matplotlib inline
+# Packages imports
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import statsmodels.stats.api as sms
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from math import ceil
+%matplotlib inline
+# Some plot styling preferences
+plt.style.use('seaborn-whitegrid')
+font = {'family' : 'Helvetica',
+        'weight' : 'bold',
+        'size'   : 14}
+
+mpl.rc('font', **font)
+
+                                                       """CALCULATING THE EFFECT SIZE"""
+effect_size = sms.proportion_effectsize(0.13, 0.15)    # Calculating effect size based on our expected rates
+required_n = sms.NormalIndPower().solve_power(
+    effect_size, 
+    power=0.8, 
+    alpha=0.05, 
+    ratio=1
+    )                                                  
+required_n = ceil(required_n)                        
+print(required_n)
 
 
                                    """Discrete Metrics"""
